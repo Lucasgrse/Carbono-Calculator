7786382104a74240978f3b3bc8e9f650f8486648
Lucas Gabriel de Resende <92735087+Lucasgrse@users.noreply.github.com>
2024-12-31 08:55:46 -0300

    feat: adiciona método update para funcionalidade

--- src/main/java/br/com/actionlabs/carboncalc/adapter/controller/dto/response/transportation/TransportationDTO.java
+++ src/main/java/br/com/actionlabs/carboncalc/adapter/controller/dto/response/transportation/TransportationDTO.java
@@ -7,4 +7,20 @@ import lombok.Data;
 public class TransportationDTO {
   private TransportationType type;
   private int monthlyDistance;
+
+  public TransportationType getType() {
+    return type;
+  }
+
+  public void setType(TransportationType type) {
+    this.type = type;
+  }
+
+  public int getMonthlyDistance() {
+    return monthlyDistance;
+  }
+
+  public void setMonthlyDistance(int monthlyDistance) {
+    this.monthlyDistance = monthlyDistance;
+  }
 }
---
+++ src/main/java/br/com/actionlabs/carboncalc/core/usecase/calculator/input/UpdateCalculatorCarbonInput.java
@@ -0,0 +1,62 @@
+package br.com.actionlabs.carboncalc.core.usecase.calculator.input;
+
+import br.com.actionlabs.carboncalc.adapter.controller.dto.response.transportation.TransportationDTO;
+
+import java.util.List;
+
+public class UpdateCalculatorCarbonInput {
+    private String id;
+    private int energyConsumption;
+    private List<TransportationDTO> transportation;
+    private int solidWasteTotal;
+    private double recyclePercentage;
+
+    public UpdateCalculatorCarbonInput(String id, int energyConsumption, List<TransportationDTO> transportation, int solidWasteTotal, double recyclePercentage) {
+        this.id = id;
+        this.energyConsumption = energyConsumption;
+        this.transportation = transportation;
+        this.solidWasteTotal = solidWasteTotal;
+        this.recyclePercentage = recyclePercentage;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getEnergyConsumption() {
+        return energyConsumption;
+    }
+
+    public void setEnergyConsumption(int energyConsumption) {
+        this.energyConsumption = energyConsumption;
+    }
+
+    public List<TransportationDTO> getTransportation() {
+        return transportation;
+    }
+
+    public void setTransportation(List<TransportationDTO> transportation) {
+        this.transportation = transportation;
+    }
+
+    public int getSolidWasteTotal() {
+        return solidWasteTotal;
+    }
+
+    public void setSolidWasteTotal(int solidWasteTotal) {
+        this.solidWasteTotal = solidWasteTotal;
+    }
+
+    public double getRecyclePercentage() {
+        return recyclePercentage;
+    }
+
+    public void setRecyclePercentage(double recyclePercentage) {
+        this.recyclePercentage = recyclePercentage;
+    }
+
+}
